fe05535662c9c2d630905af207ff86c62b640fe8
Guest User <Guest@Epicodus-2E.local>
2017-10-09 16:16:45 -0700

    Make form work

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/task/list")]
+        public ActionResult TaskList()
+        {
+            List<string> allTasks = Task.GetAll();
+            return View(allTasks);
+        }
+
+        [HttpPost("/task/create")]
+        public ActionResult CreateTask()
+        {
+            Task newTask = new Task (Request.Form["new-task"]);
+            newTask.Save();
+            return View(newTask);
+        }
+        [HttpPost("/task/list/clear")]
+        public ActionResult TaskListClear()
+        {
+            Task.ClearAll();
+            return View();
+        }
+    }
+}
